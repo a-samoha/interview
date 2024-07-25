b4a1d9dc99cc3327ee5d45392968e72d06aa9607
Artem Samoshkin <samoshkin.job@gmail.com>
2024-07-25 21:37:46 +0300

    Junior "Kotlin" ready

--- app/src/test/java/com/artsam/interview/ExampleUnitTest.kt
+++ app/src/test/java/com/artsam/interview/ExampleUnitTest.kt
@@ -1,8 +1,13 @@
 package com.artsam.interview
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +19,30 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun coroutine_test() {
+        val scope = CoroutineScope(Dispatchers.IO)
+        val job = flowOf(1, 2, 3)
+            .onEach { println("test item $it") }
+            .onCompletion { println("test onCompletion") }
+            .launchIn(scope)
+
+        assertEquals(true, job.isActive)
+    }
+
+    private fun pt() = println("test pt")
+
+    data class Tmp(private val tmp: Int)
+
+    @Test
+    fun tmp() {
+
+        val tmp = Tmp(321)
+        pt()
+        fun otherTmp() = println("test other $tmp")
+
+        otherTmp()
+    }
+
 }
